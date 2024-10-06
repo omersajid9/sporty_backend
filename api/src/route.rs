7d07b8e340e7b38b8930c6b9fd18dbd74e604bf1
omersajid9 <omersajid.777@gmail.com>
2024-10-06 16:54:27 -0400

    add player_sport handler

--- api/src/route.rs
+++ api/src/route.rs
@@ -1,14 +1,14 @@
 use axum::{routing::{get, delete, patch}, Router};
 use std::sync::Arc;
 
-use crate::{AppState, 
-    handler::{health_checker, create_player, delete_player, edit_player, get_players}
+use crate::{handler::{create_player, create_player_sport, delete_player, edit_player, get_players, health_checker}, AppState
 };
 
 pub fn create_router(app_state: Arc<AppState>) -> Router {
     Router::new()
         .route("/health_checker", get(health_checker))
         .nest("/player", player_router())
+        .nest("/player_sport", player_sport_router())
         .with_state(app_state)
 }
 
@@ -22,6 +22,7 @@ fn player_router() -> Router<Arc<AppState>> {
 
 fn player_sport_router() -> Router<Arc<AppState>> {
     Router::new()
+        .route("/create", get(create_player_sport))
         // Add routes for player sports here
         // Example: .route("/<player_id>/sport/<sport_id>", get(get_player_sport))
 }
